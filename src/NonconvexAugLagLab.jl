4f9664631f2915bc335b7774617dc80e3aaf462d
Mohamed Tarek <mohamed82008@gmail.com>
2021-09-02 00:52:00 +1000

    move from Nonconvex

--- src/NonconvexAugLagLab.jl
+++ src/NonconvexAugLagLab.jl
@@ -1,5 +1,25 @@
 module NonconvexAugLagLab
 
-# Write your package code here.
+export AugLag2, AugLag2Options
+
+import Optim
+using Reexport, Parameters, ChainRulesCore
+using Setfield, NonconvexMMA
+@reexport using NonconvexCore
+using NonconvexCore: @params, AbstractOptimizer, AbstractModel
+using NonconvexCore: ConvergenceState, ConvergenceCriteria
+using NonconvexCore: VecModel, Tolerance, AbstractFunction
+using NonconvexCore: Solution, GenericCriteria, Trace, NoCallback
+using NonconvexCore: getnineqconstraints, debugging, value_gradient
+using NonconvexCore: assess_convergence!, hasconverged, GenericResult
+
+import NonconvexCore: optimize!, Workspace, getmin, getmax, getdim
+import NonconvexCore: getobjective, getresiduals
+import NonconvexCore: getineqconstraints, geteqconstraints
+import NonconvexCore: getobjectiveconstraints
+
+include("aggregations.jl")
+include("model.jl")
+include("algorithm.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,20 @@
 using NonconvexAugLagLab
-using Test
+using Test, LinearAlgebra
 
-@testset "NonconvexAugLagLab.jl" begin
-    # Write your tests here.
-end
+f(x::AbstractVector) = sqrt(x[2])
+g(x::AbstractVector, a, b) = (a*x[1] + b)^3 - x[2]
+
+m = Model(f)
+addvar!(m, [1e-4, 1e-4], [10.0, 10.0])
+add_ineq_constraint!(
+    m,
+    FunctionWrapper(x -> [g(x, 2, 0), g(x, -1, 1)], 2),
+)
+x0 = [2.0, 2.0]
+
+#alg = AugLag2(primaloptimizer = MMA87())
+alg = AugLag2()
+options = AugLag2Options(alg)
+r = optimize(m, alg, x0, options = options)
+@test abs(r.minimum - sqrt(8/27)) < 1e-4
+@test norm(r.minimizer - [1/3, 8/27]) < 1e-4
